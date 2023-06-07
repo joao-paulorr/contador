ed0317cd1942cf53e4eb7d5cf5ba69a49ac0e061
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-07 17:12:43 +0000

    SS

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 var numero = 0
 const numeror = 0
 
-function soma() {
-    let num = document.getElementById("numero")
-    numero++
-    num.innerText = numero
-}
+// function soma() {
+//     let num = document.getElementById("numero")
+//     numero++
+//     num.innerText = numero
+// }
 
-function sub() {
-    let num = document.getElementById("numero")
-    numero--
-    num.innerText = numero
-}
+// function sub() {
+//     let num = document.getElementById("numero")
+//     numero--
+//     num.innerText = numero
+// }
 
-function resetar() {
+// function resetar() {
+//     let num = document.getElementById("numero")
+//     numero = 0
+//     num.innerText = numero
+// }
+
+function contador() {
     let num = document.getElementById("numero")
-    numero = 0
-    num.innerText = numero
 }
