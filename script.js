5e0d4b32a23f892e5cf80b5021f453af56150676
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-07 15:32:31 +0000

    z

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 var numero = 0
+const numeror = 0
 
 function soma() {
     let num = document.getElementById("numero")
-    var soma = numero++
-    num.innerText = soma
+    numero++
+    num.innerText = numero
 }
 
 function sub() {
-
+    let num = document.getElementById("numero")
+    numero--
+    num.innerText = numero
 }
 
 function resetar() {
-
+    let num = document.getElementById("numero")
+    numero = 0
+    num.innerText = numero
 }
