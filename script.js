32a25153d4f36d77e802b059f2671e0b951f5bf3
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-07 14:39:30 +0000

    h

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ var numero = 0
 
 function soma() {
     let numero = document.getElementById("num")
-
+    var soma = numero++
+    numero.innerText = soma
 }
 
 function sub() {
@@ -11,4 +12,9 @@ function sub() {
 
 function resetar() {
 
+}
+
+function incrementarContador() {
+    contador++;
+    document.getElementById("contador").innerText = contador;
 }
